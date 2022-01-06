f2c0ed28418d7af1f3cf12ba3f0c5e490e71c360
Oguzhan Celik <ogzzcelik@gmail.com>
2022-01-06 05:03:18 +0300

    Created LastActivityLayout Pages && Added Firebase Database

--- src/pages/Dashboard/LastActivities/LastActivity/LastActivity.js
+++ src/pages/Dashboard/LastActivities/LastActivity/LastActivity.js
@@ -1,10 +1,36 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import {View,Button,Text} from 'react-native'
+import LastActivityLayout from './layout/LastActivityLayout';
+import database from '@react-native-firebase/database';
 
 export default function LastActivity() {
+
+    //Sadece 1 kere getirme işlemi için
+    function checkDB(){
+        const reference = database().ref('books/');
+        reference.once('value').then(snapshot=>{
+            const response1=snapshot.val();
+            console.log(response1)
+        })
+    }
+
+    //Realtime getirmek için
+    function listenDB(){
+        const reference = database().ref('books/');
+        reference.on('value',snapshot=>{
+            const response2=snapshot.val();
+            console.log(response2)
+        })
+    }
+
+
+
     return (
         <View>
-            <Text>Last Activitys</Text>
+            <Text>Asd</Text>
+            <LastActivityLayout/>
+            <Button title='check DB' onPress={checkDB}/>
+            <Button title='Listen DB' onPress={listenDB}/>
         </View>
     )
 }
