c836275d696074bf0b5eae866de2bdf112ba522e
Oguzhan Celik <ogzzcelik@gmail.com>
2022-01-09 04:37:47 +0300

    Modal added

--- src/pages/Dashboard/NewActivity/layout/NewActivityLayout.styles.js
+++ src/pages/Dashboard/NewActivity/layout/NewActivityLayout.styles.js
@@ -1,15 +1,47 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet,Dimensions} from 'react-native';
 
+const deviceSize=Dimensions.get('window')
 export default StyleSheet.create({
     container:{
         flex:1,
+        marginBottom:20,
     },
     mapView:{
         flex:4,
     },
     buttonContainer:{
-        marginTop:30,
-        flex:1,
-        marginBottom:30,
+        marginBottom:20,
+    },
+    inner_container:{
+        borderWidth:1,
+        borderColor:'#e0e0e0',
+        padding:15
+    },
+    timer_container:{
+        marginTop:15,
+        flexDirection:'row',
+        alignItems:'center'
+    },
+    modal_container:{
+        backgroundColor:'white',
+        padding:15,
+        marginHorizontal:10,
+        borderTopLeftRadius:10,
+        borderTopRightRadius:10,
+        height:deviceSize.height/3,
+    },
+    modal:{
+        justifyContent:'flex-end',
+        margin:0,
+    },
+    distance:{
+        fontSize:20,
+        fontWeight:'bold',
+        color:'black'
+    },
+    timer:{
+        fontSize:20,
+        fontWeight:'bold',
+        color:'black'
     }
 });
